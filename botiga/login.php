5fa6a4e5eabe4c58ca1eaca4e3f1afd5b6b87782
Cristian Gonzalez Sierra <cristiangs777@gmail.com>
2023-11-18 16:55:37 +0100

    implementacio de autenticacio d'usuari
    arxius creats:
    	-login.php
    	-validacio.php

--- botiga/login.php
+++ botiga/login.php
@@ -1,3 +1,22 @@
-<?php
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+            crossorigin="anonymous"></script>
+</head>
+<body class="container bg-dark">
+<h1 class="text-center text-white">Login</h1>
+<form class="row justify-content-center" action="validacio.php">
+    <input class="col form-control mx-2" type="text" name="usuari" placeholder="usuari">
+    <input class="col form-control mx-2" type="password" name="password" placeholder="password">
+    <input class="col-12 btn btn-primary m-2" type="submit" name="submit" value="submit">
+</form>
+</div>
+</body>
+</html>
--- botiga/validacio.php
+++ botiga/validacio.php
@@ -1,5 +1,21 @@
 <?php
-import 'connexioBDD.php';
-$select = "select * ";
-$result = $conn->query($select);
+session_start();
+include 'connexioBDD.php';
+
+if (isset($_GET["usuari"]) && isset($_GET["password"])) {
+    $select = 'select * from usuari where usuari = "' . $_GET["usuari"] . '" and password = "' . $_GET["password"] . '"';
+    $result = $conn->query($select);
+    while ($row = $result->fetch_assoc()) {
+        $equalUser = $row["usuari"] === $_GET["usuari"];
+        $equalPassword = $row["password"] === $_GET["password"];
+        if($equalUser && $equalPassword){
+            $_SESSION["login"] = $_GET["usuari"] ;
+            header('Location: alta-producte.php');
+            return;
+        }
+
+    }
+
+}
+header('Location: login.php');
 ?>
